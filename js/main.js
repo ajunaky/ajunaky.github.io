c71a9ca99c917314b212c10a53b46922037b5081
Ajuna Kyaruzi <kyaruzia17@grinnell.edu>
2016-06-12 21:30:06 -0700

    Adding relevant js files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	$(".head").click(function () {
+
+		$header = $(this);
+		//getting the next element
+		$content = $header.next();
+		//open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+		$content.slideToggle(500, function () {
+			//execute this after slideToggle is done
+			//change text of header based on visibility of content div
+		    });
+
+	    });
+
+    });
